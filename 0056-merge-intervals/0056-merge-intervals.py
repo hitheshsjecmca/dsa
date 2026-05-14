e9ff241ac62f3208608d1bd769513782a5f05aab
Hithesh <hitheshsjecmca@gmail.com>
2026-05-14 09:02:11 +0530

    Time: 7 ms (97.32%), Space: 15.9 MB (73.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,11 +3,11 @@ class Solution(object):
         intervals.sort()
         merged=[]
 
-        for interval in intervals:
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
+        for i in intervals:
+            if not merged or merged[-1][1]<i[0]:
+                merged.append(i)
             else:
-                merged[-1][1]=max(merged[-1][1],interval[1])
-            
+                merged[-1][1]=max(merged[-1][1],i[1])
+
         return merged
         
