6cd5011e46e4a13c2523bb0bf7b6eb2855f5d7fb
Hithesh <hitheshsjecmca@gmail.com>
2026-08-22 10:16:54 +0530

    Time: 34 ms (59.96%), Space: 59.1 MB (59.96%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -17,8 +17,8 @@ class Solution {
                     result.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     left++;
                     right--;
-                    while(left<right && nums[left]==nums[left-1]){
-                        left++;
+                    while(left<right && nums[right]==nums[right+1]){
+                        right--;
                     }
                 }else if(sum<0){
                     left++;
